9fcf18a47a998c705959be2da66a9502f17b3617
wangcheng <wangcheng@qianjiangcloud.com>
2021-04-13 18:28:11 +0800

    2021-04-13 18:28修改

--- cloud-controller-user8010/src/main/java/com/cloud/springcloud/userservice/service/StaffService.java
+++ cloud-controller-user8010/src/main/java/com/cloud/springcloud/userservice/service/StaffService.java
@@ -1,7 +1,12 @@
 package com.cloud.springcloud.userservice.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cloud.springcloud.entities.CommonResult;
+import com.cloud.springcloud.userservice.entity.Owner;
 import com.cloud.springcloud.userservice.entity.Staff;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +18,19 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface StaffService extends IService<Staff> {
 
+    boolean saveStaff(Staff staff);
+
+    CommonResult<Page<Staff>> getStaff(Integer pn);
+
+    CommonResult<Page<Staff>> getStaffForname(String name);
+
+    CommonResult<Page<Staff>> getStaffForPhone(String phone);
+
+    CommonResult updateForId(Owner owner);
+
+    CommonResult deleteForId(int id);
+
+    CommonResult deleteForArryId(List<Integer> ids);
+
+    Owner loginin(String username,String password);
 }
--- cloud-controller-user8010/src/main/java/com/cloud/springcloud/userservice/service/impl/StaffServiceImpl.java
+++ cloud-controller-user8010/src/main/java/com/cloud/springcloud/userservice/service/impl/StaffServiceImpl.java
@@ -1,10 +1,15 @@
 package com.cloud.springcloud.userservice.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cloud.springcloud.entities.CommonResult;
+import com.cloud.springcloud.userservice.entity.Owner;
 import com.cloud.springcloud.userservice.entity.Staff;
 import com.cloud.springcloud.userservice.mapper.StaffMapper;
 import com.cloud.springcloud.userservice.service.StaffService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +21,43 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StaffServiceImpl extends ServiceImpl<StaffMapper, Staff> implements StaffService {
+    @Override
+    public boolean saveStaff(Staff staff) {
+        return false;
+    }
 
+    @Override
+    public CommonResult<Page<Staff>> getStaff(Integer pn) {
+        return null;
+    }
+
+    @Override
+    public CommonResult<Page<Staff>> getStaffForname(String name) {
+        return null;
+    }
+
+    @Override
+    public CommonResult<Page<Staff>> getStaffForPhone(String phone) {
+        return null;
+    }
+
+    @Override
+    public CommonResult updateForId(Owner owner) {
+        return null;
+    }
+
+    @Override
+    public CommonResult deleteForId(int id) {
+        return null;
+    }
+
+    @Override
+    public CommonResult deleteForArryId(List<Integer> ids) {
+        return null;
+    }
+
+    @Override
+    public Owner loginin(String username, String password) {
+        return null;
+    }
 }
